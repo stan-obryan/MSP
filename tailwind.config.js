9c2acf01d143ce673c960687b3ddf11afca3d276
stan-obryan <stan.obryan@gmail.com>
2026-07-31 22:28:38 -0400

    Adding and configuring tailwindcss
    This is a large push but this is what it took to get tailwindCSS working correctly

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./quiz.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
